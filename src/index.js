2146a492775972819ca9559a54e19447f65a3207
onuohaM <maryfeancisca@gmail.com>
2024-06-26 15:23:46 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let name = prompt('What is your name');
+let age = prompt('what is your age');
+let email =prompt ('what is your email ');
+alert(`Hi${name} you are ${age}old and we will keep in touch via ${email}`);
